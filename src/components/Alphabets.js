193c63ff5de59ddee4df1e73dc29a5c4d24b4e62
fantome-arch <69717455+fantome-arch@users.noreply.github.com>
2022-08-09 12:18:44 +0600

    Update Alphabets.js

--- src/components/Alphabets.js
+++ src/components/Alphabets.js
@@ -15,24 +15,24 @@ export default function Alphabets(props) {
 
         <div onClick={()=>{props.input('g')}}className={Normalcalc.genbtn}>g</div>
         <div onClick={()=>{props.input('h')}}className={Normalcalc.genbtn}>h</div>
-        <div onClick={()=>{props.input('i')}}className={Normalcalc.genbtn}>j</div>
+        <div onClick={()=>{props.input('j')}}className={Normalcalc.genbtn}>j</div>
 
-        <div onClick={()=>{props.input('j')}}className={Normalcalc.genbtn}>k</div>
-        <div onClick={()=>{props.input('k')}}className={Normalcalc.genbtn}>l</div>
-        <div onClick={()=>{props.input('l')}}className={Normalcalc.genbtn}>m</div>
+        <div onClick={()=>{props.input('k')}}className={Normalcalc.genbtn}>k</div>
+        <div onClick={()=>{props.input('l')}}className={Normalcalc.genbtn}>l</div>
+        <div onClick={()=>{props.input('m')}}className={Normalcalc.genbtn}>m</div>
       </div>
       <div className={Normalcalc.numpad}>
-        <div onClick={()=>{props.input('m')}}className={Normalcalc.genbtn}>n</div>
-        <div onClick={()=>{props.input('n')}}className={Normalcalc.genbtn}>o</div>
-        <div onClick={()=>{props.input('o')}}className={Normalcalc.genbtn}>p</div>
+        <div onClick={()=>{props.input('n')}}className={Normalcalc.genbtn}>n</div>
+        <div onClick={()=>{props.input('o')}}className={Normalcalc.genbtn}>o</div>
+        <div onClick={()=>{props.input('p')}}className={Normalcalc.genbtn}>p</div>
 
-        <div onClick={()=>{props.input('p')}}className={Normalcalc.genbtn}>q</div>
-        <div onClick={()=>{props.input('q')}}className={Normalcalc.genbtn}>r</div>
-        <div onClick={()=>{props.input('r')}}className={Normalcalc.genbtn}>s</div>
+        <div onClick={()=>{props.input('q')}}className={Normalcalc.genbtn}>q</div>
+        <div onClick={()=>{props.input('r')}}className={Normalcalc.genbtn}>r</div>
+        <div onClick={()=>{props.input('s')}}className={Normalcalc.genbtn}>s</div>
 
-        <div onClick={()=>{props.input('s')}}className={Normalcalc.genbtn}>t</div>
-        <div onClick={()=>{props.input('t')}}className={Normalcalc.genbtn}>u</div>
-        <div onClick={()=>{props.input('u')}}className={Normalcalc.genbtn}>v</div>
+        <div onClick={()=>{props.input('t')}}className={Normalcalc.genbtn}>t</div>
+        <div onClick={()=>{props.input('u')}}className={Normalcalc.genbtn}>u</div>
+        <div onClick={()=>{props.input('v')}}className={Normalcalc.genbtn}>v</div>
 
         <div onClick={()=>{props.input('w')}}className={Normalcalc.genbtn}>w</div>
       </div>
